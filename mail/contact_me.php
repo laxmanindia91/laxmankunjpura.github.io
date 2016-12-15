262988413153821224484baff4c39c1c8e8bad8e
laxmankunjpura <laxman@netscriptindia.com>
2016-12-15 12:24:04 +0530

    send mail change

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,8 +19,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'laxman091@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@laxmankunjpura.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers = "From: noreply@laxmankunjpura.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@laxmankunjpura.github.io
+$headers .= "Reply-To: $email_address";
+// send email
+mail("someone@example.com","Test mail","Test Message"); 
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
